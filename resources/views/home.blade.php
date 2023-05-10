d03689020a1dfd6dec3ace7e39e5a44ccdbb1d5e
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-10 17:13:14 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/main')
+
+@section('content')
+  
+<h1>Database Film</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Guest\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// aprire array inserire la classe del controller 
+// inserire la , poi il nome della funzione da chiamare
+Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::get('/movies', [PageController::class, 'index'])->name('index');
+
+    
+
